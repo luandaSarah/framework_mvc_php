0f8d09fadb4dc31b11f9b5f362fe5c7f376ce553
kaimon0 <luandasarah1031@gmail.com>
2025-04-18 13:08:20 +0200

    [MAJ]

--- app/Views/Layout/_header.php
+++ app/Views/Layout/_header.php
@@ -1,7 +1,7 @@
 <header class="sticky-top">
     <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="/">My first PHP MVC</a>
+            <a class="navbar-brand" href="/">Mini Indeed</a>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"></button>
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
